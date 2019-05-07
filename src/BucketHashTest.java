3e72cb3984a0552c299619e56377e568c1f7f112
Yannik Sood <yanniksood7@gmail.com>
2019-05-07 15:28:25 -0400

    Bucket hash insert donezo

--- src/BucketHashTest.java
+++ src/BucketHashTest.java
@@ -31,43 +31,8 @@ public class BucketHashTest extends TestCase {
         assertEquals(0, hTable.insert("TTTTTTTTTTAAAAACCCA", m1, m2));
         assertEquals(62, hTable.insert("TCATATCTATCCAAAAAAAA", m1, m2));
         assertEquals(63, hTable.insert("TCATATCTATCCAAAAAAA", m1, m2));
-        //assertEquals(32, hTable.insert("TCATATCTATCCAAAAAA", m1, m2));
-        assertEquals(6, hTable.getSize());
+        assertEquals(32, hTable.insert("TCATATCTATCCAAAAAA", m1, m2));
+        assertEquals(7, hTable.getSize());
     }
     
-    /**
-     * Test Insert with edge cases - full, duplicates, and tombstones
-     */
-    public void testInsertFull() {
-        //Test Full HT
-        hTable = new BucketHash(5);
-        assertEquals(0, hTable.getSize());
-        assertEquals(1, hTable.insert("AAAAA", m1, m2));
-        assertEquals(2, hTable.insert("AAAA", m1, m2));
-        assertEquals(3, hTable.insert("AAA", m1, m2));
-        assertEquals(4, hTable.insert("TCATATCTATCCAAAAAAAA", m1, m2));
-        assertEquals(0, hTable.insert("TTTTTTTTTTAAAAACCCA", m1, m2));
-        assertEquals(-1, hTable.insert("TCATATCTATCCAAAAAAA", m1, m2));
-    }
-    
-    /**
-     * Test Insert with edge cases - full, duplicates, and tombstones
-     */
-    public void testInsertTomb() {
-        //Test Full HT
-        hTable = new BucketHash(5);
-        assertEquals(0, hTable.getSize());
-        assertEquals(1, hTable.insert("AAAAA", m1, m2));
-        assertEquals(2, hTable.insert("AAAA", m1, m2));
-        assertEquals(3, hTable.insert("AAA", m1, m2));
-        assertEquals(4, hTable.insert("TCATATCTATCCAAAAAAAA", m1, m2));
-        assertEquals(0, hTable.insert("TTTTTTTTTTAAAAACCCA", m1, m2));
-        assertEquals(-1, hTable.insert("TCATATCTATCCAAAAAAA", m1, m2));
-        hTable.insertTomb(0);
-        assertEquals(0, hTable.insert("TTTTTTTTTTAAAAACCCA", m1, m2));
-        hTable.insertTomb(1);
-        assertEquals(1, hTable.insert("AAAAA", m1, m2));
-        
-        //Test Duplicate when Search Done
-    }
 }
--- src/DNADataBase.java
+++ src/DNADataBase.java
@@ -7,72 +7,145 @@ import java.io.IOException;
  * sequence ID. It also includes a memory manager to help with binary file
  * storage of sequences and sequence ID's.
  * 
- * @author  adaniel1 (Daniel Almeida) & yannik24 (Yannik Sood)
+ * @author adaniel1 (Daniel Almeida) & yannik24 (Yannik Sood)
  * @version 5/5/19
  */
 public class DNADataBase {
     private MemManager memManager; // memory manager
     private BucketHash bHash;
 
+
     /**
      * Default constructor for DNA Database.
      * 
-     * @param   fileName                name of binary file
-     * @throws IOException 
+     * @param fileName
+     *            name of binary file
+     * @throws IOException
      */
     public DNADataBase(String fileName, int size) throws IOException {
         this.memManager = new MemManager(fileName);
         this.bHash = new BucketHash(size);
     }
 
+
     /**
      * Insert a sequence of length len and associated sequence ID
      * into memory manager and hash-table.
      * 
-     * @param seqID     sequence's ID
-     * @param len       sequence's length
-     * @param seq       actual sequence
+     * @param seqID
+     *            sequence's ID
+     * @param len
+     *            sequence's length
+     * @param seq
+     *            actual sequence
      * 
      * @throws IOException
      * @throws NumberFormatException
      */
     public void insert(String seqID, String len, String seq)
-        throws NumberFormatException, IOException {
-        
+        throws NumberFormatException,
+        IOException {
+
         // memory manager insertion
-        memManager.insert(seqID, seqID.length()); // insert ID
-        memManager.insert(seq, Integer.parseInt(len)); // insert sequence
-        
-        // hash table insertion
-        
+        // insert
+        // sequence
+
+        // Check for duplicates
+        if (!this.search(seqID)) {
+            MemHandle idHandle = memManager.insert(seqID, seqID.length()); // insert
+            MemHandle seqHandle = memManager.insert(seq, Integer.parseInt(len));
+            int slot = bHash.insert(seqID, idHandle, seqHandle);
+            if (slot > -1) {
+                // Print positive output
+            }
+            else {
+                // insertion declined, print rejection output
+            }
+        }
+        else {
+            //Print rejection state
+        }
     }
-    
+
+
     /**
-     * Insert a sequence of length len and associated sequence ID
-     * into memory manager and hash-table.
+     * Remove a sequence from memory manager and hash-table.
      * 
-     * @param seqID     sequence's ID
-     * @param len       sequence's length
-     * @param seq       actual sequence
+     * @param seqID
+     *            sequence's ID
      * 
      * @throws IOException
      * @throws NumberFormatException
      */
-    public void remove(String seqID) 
-        throws NumberFormatException, IOException {
-        
-        // memory manager insertion
-        
-        // hash table insertion
+    public void remove(String seqID) throws NumberFormatException, IOException {
+
+        // hash table remove
+        int i = 0;
+        int max = bHash.getMaxSize();
+        while (i < max) {
+            TableEntry temp = bHash.get(i); //peek table entry at i
+            
+            if (temp.getID() != null && temp.getSequence() != null && temp
+                .getSlot() != -1) {
+                // Get bits and convert to string. Compare and if found, insert
+                // a tombstone
+                // At I and print output.
+            }
+        }
+
+    }
+
+
+    /**
+     * Search a sequence in memory manager and hash-table.
+     * 
+     * @param seqID
+     *            sequence's ID
+     * 
+     * @throws IOException
+     * @throws NumberFormatException
+     */
+    public boolean search(String seqID)
+        throws NumberFormatException,
+        IOException {
+
+        // hash table remove
         int i = 0;
         int max = bHash.getMaxSize();
         while (i < max) {
             TableEntry temp = bHash.get(i);
-            if (temp.getID() != null && temp.getSequence() != null && temp.getSlot() != -1) {
-                //Get ID and 
+            if (temp.getID() != null && temp.getSequence() != null && temp
+                .getSlot() != -1) {
+                // Get bits from mem manager and convert to string, then compare
+                // to sequence ID. if found, print output.
+                return true;
             }
         }
-        
+        return false;
     }
-    
+
+
+    /**
+     * Print all sequences in memory manager and hash-table.
+     * 
+     * 
+     * @throws IOException
+     * @throws NumberFormatException
+     */
+    public void print() throws NumberFormatException, IOException {
+
+        // hash table remove
+        int i = 0;
+        int max = bHash.getMaxSize();
+        while (i < max) {
+            TableEntry temp = bHash.get(i);
+            if (temp.getID() != null && temp.getSequence() != null && temp
+                .getSlot() != -1) {
+                // Get ID and pass into Mem Man, then convert to string and
+                // print
+            }
+        }
+
+    }
+
 }
